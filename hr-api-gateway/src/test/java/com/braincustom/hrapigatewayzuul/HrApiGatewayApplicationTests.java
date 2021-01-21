d650dba141033aac7934f8810db14561ac389e21
hvalmer <hvalmer@gmail.com>
2021-01-20 23:50:50 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway/src/test/java/com/braincustom/hrapigatewayzuul/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincustom.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
