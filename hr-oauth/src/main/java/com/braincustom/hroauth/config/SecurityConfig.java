efe0a120dbbe4296bf2dc29c051ab08e39f9bb41
hvalmer <hvalmer@gmail.com>
2021-01-31 19:54:32 -0300

    Login JWT token

---
+++ hr-oauth/src/main/java/com/braincustom/hroauth/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.braincustom.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*Configurando a autenticação...é quando a aplicação recebe as credenciais do usuário 
+ * e chama o usuário pelo username do BD do microsserviço de User, comparar a senha
+ * e vê se bateu, daí autenticar o usuário e devolver o token
+ *  */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	//sobre escrevendo o método da classe WebSecurityConfigurerAdapter
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+}
--- hr-oauth/src/main/java/com/braincustom/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/braincustom/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.braincustom.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.braincustom.hroauth.entities.User;
 import com.braincustom.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found!");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	//Implementando o Método do Spring Security
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.finByEmail(username).getBody(); //comunicar com o ms de usuários
+		if(user == null) { //tratamento de erro caso não encontre o email
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found!");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
